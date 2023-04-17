77df3c1abe66cf7de0242a363c40d1c3b60fb685
tejaslakade <tejaslakade99@gmail.com>
2023-04-17 09:54:04 +0530

    Security Changes.

--- TimeZone/settings.py
+++ TimeZone/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3_=!q0ck#njp1d9m^d3u4%k+a7+ya+mq9ynao0zyubxk5@g@c)'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -136,5 +136,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'oliverknight034@gmail.com'
-EMAIL_HOST_PASSWORD = 'wphwzjvlwlwmfjbt'
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
