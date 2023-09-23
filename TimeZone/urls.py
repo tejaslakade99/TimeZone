62d079502039f4d3d94e4698664a3be4b0a0f224
Tejas Lakade <69455769+tejaslakade99@users.noreply.github.com>
2023-09-23 15:00:13 +0530

    Update urls.py

--- TimeZone/urls.py
+++ TimeZone/urls.py
@@ -21,5 +21,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('TimeZone/', include('TimeZoneApp.urls'))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] 
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
 
