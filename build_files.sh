fe7c91e459f34ba13ac535b68a14c284f9212557
Tejas Lakade <69455769+tejaslakade99@users.noreply.github.com>
2023-09-23 23:41:48 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,10 +1,4 @@
 echo "BUILD START"
 python3.9 -m pip install -r requirements.txt
 python3.9 manage.py collectstatic --noinput --clear
-
-echo "MAKE MIGRATIONS"
-python3.9 manage.py makemigrations
-python3.9 manage.py migrate
-echo "END MIGRATIONS"
-
 echo "BUILD END"
