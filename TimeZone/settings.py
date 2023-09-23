c95b1f8f49bf644a8d3b0281c58dbbd24d3ded8d
Tejas Lakade <69455769+tejaslakade99@users.noreply.github.com>
2023-09-23 16:33:15 +0530

    Update settings.py

--- TimeZone/settings.py
+++ TimeZone/settings.py
@@ -128,10 +128,8 @@ USE_TZ = False
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 
 ## For Vercel Hosting Platform
-STATIC_URL = os.path.join(BASE_DIR,'static')
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles_build', 'static')]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / "staticfiles_build" / "static"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
