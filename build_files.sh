b6f12bd742ce5b7e7fa45ac7bd444ad287a5616d
tejaslakade99 <tejaslakade99@gmail.com>
2023-09-24 13:03:36 +0530

    For Hosting Project on Vercel

--- build_files.sh
+++ build_files.sh
@@ -7,4 +7,6 @@ python3.9 manage.py makemigrations
 python3.9 manage.py migrate
 echo "MIGRATION FINISHED"
 
+python3.9 manage.py createsuperuser --username="Tejas" --email="tejaslakade99@gmail.com"
+
 echo "BUILD END"
