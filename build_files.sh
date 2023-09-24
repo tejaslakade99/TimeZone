50663020604ad8b0103ca3340b93a04f8a7906ed
tejaslakade99 <tejaslakade99@gmail.com>
2023-09-24 11:18:32 +0530

    For Hosting Project on Vercel

--- build_files.sh
+++ build_files.sh
@@ -4,7 +4,7 @@ python3.9 manage.py collectstatic --noinput --clear
 
 echo "MIGRATION STARTED"
 python3.9 manage.py makemigrations
-python3.9 manage.oy migrate
+python3.9 manage.py migrate
 echo "MIGRATION FINISHED"
 
 echo "BUILD END"
